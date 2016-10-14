d7e9d4887da12b82675ab4d09dab0178a64e4f18
JAukey <jackaukey@126.com>
2016-10-14 14:43:00 +0800

    commit

--- luasrc/controller/bashportal.lua
+++ luasrc/controller/bashportal.lua
@@ -1,5 +1,16 @@
 module("luci.controller.bashportal", package.seeall)
-function index()
-		entry({"admin", "network", "bashportal"},cbi("bashportal/base"),_("Bashportal"), 60)
-end
 
+function index()
+	
+	entry({"admin", "network", "bashportal"},
+		alias("admin", "network", "bashportal", "base"),
+		_("Bashportal"), 60)
+
+	entry({"admin", "network", "bashportal","base"},
+		cbi("bashportal/base"),
+		_("Bashportal Base"), 10).leaf = true
+
+	entry({"admin", "network", "bashportal", "filetransfer"},
+		cbi("bashportal/filetransfer"),
+		_("File Transfer"), 20).leaf = true
+end
--- luasrc/model/cbi/bashportal/base.lua
+++ luasrc/model/cbi/bashportal/base.lua
@@ -1,6 +1,7 @@
 require("luci.sys")
 m = Map("bashportal", translate("Bash Portal"), translate("Emerge Bash Portal Project."))
-s = m:section(TypedSection,"bashportal")
+s = m:section(TypedSection,"base",translate("Base Setting"))
+s.template = "cbi/bspsection"
 s.addremove = false
 s.anonymous = true
 s:option(Flag, "enabled", translate("Enable"))
@@ -9,8 +10,12 @@ s:option(Value, "portalpath", translate("Portal Path"))
 s:option(Value, "luafile", translate("Lua File"))
 s:option(Value, "ngxconf", translate("Nginx Configuration File"))
 s:option(Value, "interfaces", translate("Interfaces"),translate("Interface should be separate by ' , ',like 'br-lan,eth0'."))
+s:option(Flag, "activityen", translate("Activity Time Control Enable"))
+s:option(Value, "activitytime", translate("Activity Time Set"))
+
 local apply = luci.http.formvalue("cbi.apply")
 if apply then
      io.popen("/etc/init.d/bashportal restart")
+     io.popen("/etc/init.d/injection restart")
 end
 return m
